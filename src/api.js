19bfb092e1b311bd2603ba2ca10dfc5eda779b43
chaitanya <chaitanyaagarkar11@gmail.com>
2023-08-06 21:28:33 +0530

    Initial changes

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8030fec9d4mshf31dcd1a76a463fp134421jsna7cc76575787",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
